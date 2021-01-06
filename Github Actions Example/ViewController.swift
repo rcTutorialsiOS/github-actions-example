b7a42acd876a1e15070b6a7f6323bc21993b0408
Ricardo <ralgoritmo@gmail.com>
2021-01-06 20:33:30 +0100

    CI workflow

--- Github Actions Example/ViewController.swift
+++ Github Actions Example/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad() daohdoiashoidahsio
         // Do any additional setup after loading the view.
     }
 
